8d4ebb5544327b562ef98124d6ec6b6f73804852
NegiSushant <sushantsinghnegi1998@gmail.com>
2025-01-17 13:51:17 +0530

    added

--- initial.py
+++ initial.py
@@ -1,14 +1,21 @@
 from autogen_ext.models.openai import AzureOpenAIChatCompletionClient
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
-
+from dotenv import load_dotenv
+import os
 #create the token provider
-token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
+
+load_dotenv()
+endpoint = os.getenv('AZURE_ENDPOINT')
+Api_key = os.getenv('API_KEY')
+deployment = os.getenv('DEPLOYMENT_NAME')
+vesrion = os.getenv('OPENAI_API_VERSION')
+
 
 
 client = AzureOpenAIChatCompletionClient( 
-    token_provider=token_provider,
+    
     azure_depoyment_id="your_deployment_id",
-    model="",
-    api_version= "",
-    azure_endpoint="https://your_endpoint.cognitiveservices.azure.com",                                     
+    model="gpt-4o",
+    api_version= vesrion,
+    azure_endpoint= endpoint,                                     
     )
