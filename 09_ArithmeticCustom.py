fcd431645c2dd0c06b795ad638b827363ab9967a
NegiSushant <sushantsinghnegi1998@gmail.com>
2025-01-28 10:20:40 +0530

    custom arithmetic agent

--- 09_ArithmeticCustom.py
+++ 09_ArithmeticCustom.py
@@ -11,7 +11,7 @@ from Client_initialization import client
 
 
 class ArithmeticAgent(BaseChatAgent):
-    def __intit__(self, name: str, description: str, operator_func: Callable[[int], int]) -> None:
+    def __init__(self, name: str, description: str, operator_func: Callable[[int], int]) -> None:
         super().__init__(name, description=description)
         self._operator_func = operator_func
         self._message_history: List[ChatMessage] = []
@@ -19,28 +19,25 @@ class ArithmeticAgent(BaseChatAgent):
     @property
     def produced_message_types(self) -> Sequence[type[ChatMessage]]:
         return (TextMessage,)
-    
-    async def on_messages(self, messages: Sequence[ChatMessage], cancellation_token: CancellationToken)-> Response:
-        #update the message history.
-        #Note: it is possible the messages is empty list, which means the agent was selected previously.
+
+    async def on_messages(self, messages: Sequence[ChatMessage], cancellation_token: CancellationToken) -> Response:
+        # Update the message history.
+        # NOTE: it is possible the messages is an empty list, which means the agent was selected previously.
         self._message_history.extend(messages)
-        #parse the number in the last message
+        # Parse the number in the last message.
         assert isinstance(self._message_history[-1], TextMessage)
         number = int(self._message_history[-1].content)
-
-        #Apply the operator function to the number
+        # Apply the operator function to the number.
         result = self._operator_func(number)
-        #create a new message with the result
-        response_message = TextMessage(content= str(result), source=self.name)
-        #Update the message hitory
+        # Create a new message with the result.
+        response_message = TextMessage(content=str(result), source=self.name)
+        # Update the message history.
         self._message_history.append(response_message)
-        
-        #return the response
+        # Return the response.
         return Response(chat_message=response_message)
 
     async def on_reset(self, cancellation_token: CancellationToken) -> None:
         pass
-
 
 async def run_number_agents() -> None:
     #create agents for number operations
