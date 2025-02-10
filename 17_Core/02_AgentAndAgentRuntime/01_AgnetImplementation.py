7ebffe8ecf1682cefaea335ec057120e23cf055b
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2025-02-10 21:35:09 +0530

    Adding register into custom agent

--- 17_Core/02_AgentAndAgentRuntime/01_AgnetImplementation.py
+++ 17_Core/02_AgentAndAgentRuntime/01_AgnetImplementation.py
@@ -2,10 +2,24 @@ from dataclasses import dataclass
 from autogen_core import AgentId, MessageContext, RoutedAgent, message_handler, AgentRuntime
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.messages import TextMessage
-from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_ext.models.openai import AzureOpenAIChatCompletionClient
 import asyncio
 from autogen_core import SingleThreadedAgentRuntime
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+endpoint = os.getenv("AZURE_ENDPOINT")
+Api_key = os.getenv("API_KEY")
+apiVersion = os.getenv("OPENAI_API_VERSION")
+
+client = AzureOpenAIChatCompletionClient(
+    azure_depoyment_id="gpt-4o",
+    model="gpt-4o",
+    api_version= apiVersion,
+    azure_endpoint=endpoint,
+    api_key = Api_key,
+)
 
 @dataclass
 class MyMessageType:
@@ -19,11 +33,11 @@ class MyAgent(RoutedAgent):
     async def handle_my_message_type(self, message: MyMessageType, ctx: MessageContext) -> None:
         print(f"{self.id.type} received message: {message.content}")
 
-
+#Wrapper agent for the AssistantAgent in AgentChat
 class MyAssistant(RoutedAgent):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        model_client = OpenAIChatCompletionClient(model="gpt-4o")
+        model_client = client
         self._delegate = AssistantAgent(name, model_client=model_client)
 
     @message_handler
@@ -35,10 +49,22 @@ class MyAssistant(RoutedAgent):
         print(f"{self.id.type} responded: {response.chat_message.content}")
 
 async def main():
-    agent = MyAgent()
-    message = await agent.handle_my_message_type(MyMessageType(message="hello! world,"), ctx=agent.id)
+    # agent = MyAgent()
+    # message = await agent.handle_my_message_type(MyMessageType(message="hello! world,"), ctx=agent.id)
 
-    print(message)
+    runtime = SingleThreadedAgentRuntime()
+
+    #register our agent types with the SingleThreadedAgentRuntime()
+    await MyAgent.register(runtime, "my_agent", lambda: MyAgent())
+    await MyAssistant.register(runtime, "my_assistant", lambda: MyAssistant("my_assistant"))
+
+    # start processing messages in the background
+    runtime.start()
+
+    await runtime.send_message(MyMessageType("Hello, World!"), AgentId("my_agent", "default"))
+    await runtime.send_message(MyMessageType("Hello, Negi!"), AgentId("my_assistant", "default"))
+    await runtime.stop()
+    # print(message)
     # async with SimpleAgentRuntime() as runtime:
     #     agent = await runtime.create_agent(MyAgent)
 
