8d7134a0090ef4d211d68f0f8aeb1ab27cd038c5
NegiSushant <sushantsinghnegi1998@gmail.com>
2025-02-11 12:12:31 +0530

    remove dupliates

--- 17_Core/02_AgentAndAgentRuntime/01_AgnetImplementation.py
+++ 17_Core/02_AgentAndAgentRuntime/01_AgnetImplementation.py
@@ -22,19 +22,6 @@ client = AzureOpenAIChatCompletionClient(
     api_key = Api_key,
 )
 
-load_dotenv()
-endpoint = os.getenv("AZURE_ENDPOINT")
-Api_key = os.getenv("API_KEY")
-apiVersion = os.getenv("OPENAI_API_VERSION")
-
-client = AzureOpenAIChatCompletionClient(
-    azure_depoyment_id="gpt-4o",
-    model="gpt-4o",
-    api_version= apiVersion,
-    azure_endpoint=endpoint,
-    # AZURE_OPENAI_API_KEY = Api_key
-    api_key = Api_key,
-)
 
 @dataclass
 class MyMessageType:
