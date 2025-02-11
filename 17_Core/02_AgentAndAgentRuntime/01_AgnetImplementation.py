d1de7c54a133b479b6a45ee0dbc4a8c1e88654e1
NegiSushant <sushantsinghnegi1998@gmail.com>
2025-02-11 12:00:00 +0530

    changes

--- 17_Core/02_AgentAndAgentRuntime/01_AgnetImplementation.py
+++ 17_Core/02_AgentAndAgentRuntime/01_AgnetImplementation.py
@@ -2,10 +2,26 @@ from dataclasses import dataclass
 from autogen_core import AgentId, MessageContext, RoutedAgent, message_handler, AgentRuntime
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.messages import TextMessage
-from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_ext.models.openai import AzureOpenAIChatCompletionClient
 import asyncio
 from autogen_core import SingleThreadedAgentRuntime
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
+endpoint = os.getenv("AZURE_ENDPOINT")
+Api_key = os.getenv("API_KEY")
+apiVersion = os.getenv("OPENAI_API_VERSION")
+
+client = AzureOpenAIChatCompletionClient(
+    azure_depoyment_id="gpt-4o",
+    model="gpt-4o",
+    api_version= apiVersion,
+    azure_endpoint=endpoint,
+    # AZURE_OPENAI_API_KEY = Api_key
+    api_key = Api_key,
+)
 
 @dataclass
 class MyMessageType:
@@ -23,7 +39,7 @@ class MyAgent(RoutedAgent):
 class MyAssistant(RoutedAgent):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        model_client = OpenAIChatCompletionClient(model="gpt-4o")
+        model_client = client
         self._delegate = AssistantAgent(name, model_client=model_client)
 
     @message_handler
@@ -34,19 +50,17 @@ class MyAssistant(RoutedAgent):
         )
         print(f"{self.id.type} responded: {response.chat_message.content}")
 
+runtime = SingleThreadedAgentRuntime()
+
 async def main():
-    agent = MyAgent()
-    message = await agent.handle_my_message_type(MyMessageType(message="hello! world,"), ctx=agent.id)
-
-    print(message)
-    # async with SimpleAgentRuntime() as runtime:
-    #     agent = await runtime.create_agent(MyAgent)
-
-    #     # Simulate sending a message to the agent
-    #     message = MyMessageType(content="Hello, Agent!")
-    #     ctx = MessageContext(agent.id)
-
-    #     await agent.handle_my_message_type(message, ctx)
+    myAgent = await MyAgent.register(runtime, "my_agent", lambda: MyAgent())
+    print(myAgent)
+    myAssistant = await MyAssistant.register(runtime, "my_assistant", lambda:MyAssistant("my_assistant"))
+    print(myAssistant)
+    runtime.start()  # Start processing messages in the background.
+    await runtime.send_message(MyMessageType("Hello, World!"), AgentId("my_agent", "default"))
+    await runtime.send_message(MyMessageType("Hello, World!"), AgentId("my_assistant", "default"))
+    await runtime.stop()  # Stop processing messages in the background.
 
 
 if __name__ == "__main__":
