1b6d6565a347bf48539db449bbb3eeeb10ca1744
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2025-03-04 22:12:00 +0530

    model client ops

---
+++ 18_model client/ModelClientAAD.py
@@ -0,0 +1,15 @@
+from autogen_ext.models.openai import AzureOpenAIChatCompletionClient
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+
+
+#create the token provider
+token_pro = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
+
+az_model_client = AzureOpenAIChatCompletionClient(
+    azure_deployment="{deployement}",
+    model="gpt-4o",
+    api_version="2024-06-01",
+    azure_endpoint="https://{}.openai.azure.com/",
+    azure_ad_token_provider=token_pro
+)
+
---
+++ 18_model client/modelClient.md
@@ -0,0 +1,8 @@
+### All model clients implement the ChatCompletionClient
+* Currently there are 3 built in model Client:
+* OpenAIChatCompletionClient
+* AzureOpenAIChatCompletionClient
+* AzureAIChatCompletionClient
+
+create() method is used to create a chat completion request, and await for an CreateResult object in return.
+
